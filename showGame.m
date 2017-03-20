979233b002a3720578c2cfcd8d174944b72a1600
Igor Oliveira Borges <igor.borges@hotmail.com>
2017-03-20 15:41:08 -0300

    Update showGame.m

--- showGame.m
+++ showGame.m
@@ -1,9 +1,15 @@
+%% This a function of embed Graphical engine used optionally in simulator D2DSS
+% It is responsable for plotting variable graphicals structures of soccer game
+% Score, iteration time, labels, ball position, players position of game simulation
+% with aspects defined for parameters from State, setSimulator[SimulatorCore], setGame[J]
+%      function showGame( S, M, J )
+%%
 function showGame( S, M, J )
 
        	pause(M.delay);
         clf;
 
-    %PARTE DO CAMPO
+    %Field personalization
     subplot(10,5,[6,50]);
    
     axis on;grid on;
@@ -16,17 +22,17 @@ function showGame( S, M, J )
     set(gca, 'YTickLabelMode', 'manual', 'YTickLabel', []);
 
 
-    % AREA DO CAMPO    
+    %Colorizing 
     field=rectangle('position',[(0)*M.cellWidth+.0 (0)*M.cellWidth+.0 (M.cellWidth*M.Nx) (M.cellWidth*M.Ny)],'LineWidth',.1);
-    set(field,'FaceColor',[0.0,1.0,0.0]); %VALDINEI
+    set(field,'FaceColor',[0.0,1.0,0.0]);
 
-    %AREAS DOS GOLS
+    %Goals areas
     golA=rectangle('position',[0 (floor(M.Ny/2)*M.cellWidth-M.cellWidth*M.goalWidth) M.cellWidth M.cellWidth*(M.goalWidth*2+1)],'LineWidth',2);
-    set(golA,'FaceColor',[0.8,0.8,0.8]); %VALDINEI
+    set(golA,'FaceColor',[0.8,0.8,0.8]);
     golB=rectangle('position',[M.Nx*M.cellWidth-M.cellWidth (floor(M.Ny/2)*M.cellWidth-M.cellWidth*M.goalWidth) M.cellWidth M.cellWidth*(M.goalWidth*2+1)],'LineWidth',2);
-    set(golB,'FaceColor',[0.4,0.4,0.4]); %VALDINEI
+    set(golB,'FaceColor',[0.4,0.4,0.4]);
     
-    %Jogadores timeA
+    %Players of team A
     if(M.Ta>0)
         for i=1:M.Ta
             player=rectangle('position',[(S.P{i}.x-1)*M.cellWidth+.5 (S.P{i}.y-1)*M.cellWidth+.5 (M.cellWidth-1) (M.cellWidth-1)],'LineWidth',.1);
@@ -34,7 +40,7 @@ function showGame( S, M, J )
             text((S.P{i}.x-1)*M.cellWidth+1,(S.P{i}.y-1)*M.cellWidth+1,num2str(i));
         end
     end
-    %Jogadores timeB
+    %Players of team B
     if (M.Tb>0)
         for i=M.Ta+1:M.Ta+M.Tb
             player=rectangle('position',[(S.P{i}.x-1)*M.cellWidth+.5 (S.P{i}.y-1)*M.cellWidth+.5 (M.cellWidth-1) (M.cellWidth-1)],'LineWidth',.1);
@@ -46,7 +52,7 @@ function showGame( S, M, J )
     ball=rectangle('position',[(S.B.x-1)*M.cellWidth+1 (S.B.y-1)*M.cellWidth+1 (M.cellWidth-2) (M.cellWidth-2)],'LineWidth',.1);
     set(ball,'FaceColor',[1,1,1]); %VALDINEI
 
-    %CIRCULO CENTRAL
+    %Central circle
     hold on;
     numPoints=100;
     radius=M.cellWidth*1;
@@ -55,9 +61,10 @@ function showGame( S, M, J )
     [X,Y] = pol2cart(theta,rho); %funcao circular
     plot(X+M.Nx/2*M.cellWidth,Y+M.Ny/2*M.cellWidth,'k-','linewidth',2);
 
-    %LINHA CENTRAL   
+    %Central mid line
     line([M.Nx/2*M.cellWidth,M.Nx/2*M.cellWidth],[0,M.Ny*M.cellWidth],'Color','k');    
     
+    %Labels for naming, score and time
     subplot(10,5,[1,5]);
     axis off;
     titulo='D2DSS - Discrete 2D Soccer Simulator';
