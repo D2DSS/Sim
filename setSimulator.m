8e3352f12b8c14f4ba1dd57434d7e1ea88c794fa
Igor Oliveira Borges <igor.borges@hotmail.com>
2017-03-20 17:27:22 -0300

    Update setSimulator.m

--- setSimulator.m
+++ setSimulator.m
@@ -1,7 +1,9 @@
+%This class sets up variables for parameters of simulation of games in simulator D2DSS
+%	function M = setSimulator(Nx, Ny, Ta, Tb, randAfterGoal)
+% Responsable for vinculates main variables of simulation in M structure
 function M = setSimulator(Nx, Ny, Ta, Tb, randAfterGoal)
-%This class sets up variables for parameters of simulation of games
 
-	%Probabilities for simulation
+    %Probabilities for simulation
     M.ProbBallWithPlayer = 0.9; %sets probability for sucessfully carry the ball (with ball)
     M.ProbPlayerMove= 0.95; %sets probability for player move (without ball)
     M.ProbTackle= 0.5; %probability for recovery ball in a tackle (player needed to be at 4 neer positions xy where ball is)
@@ -9,10 +11,9 @@ function M = setSimulator(Nx, Ny, Ta, Tb, randAfterGoal)
     M.ProbBallMoveDiagonal=0.5; %probability of ball disrupts its own direction
     
     %Fuctions denotated and used by simulator
-    M.MaxBallVelocity= max(1,floor((-1+sqrt(1+Nx*8*1/3))/2)); % a velocidade decai em 1 a cada iteracao
-    M.KickRange = ((M.MaxBallVelocity*(M.MaxBallVelocity+1))/2); %aqui � calculado quanto que o chute vai percorrer
-    M.goalWidth = floor(Ny/6);
-    
+    M.MaxBallVelocity= max(1,floor((-1+sqrt(1+Nx*8*1/3))/2)); %velocity is decremented one in each iteration
+    M.KickRange = ((M.MaxBallVelocity*(M.MaxBallVelocity+1))/2); %calculates max range of a kick
+    M.goalWidth = floor(Ny/6); %size of goal
     
     %Graphical Configurations
     M.cellWidth = 3; %size of a cell
@@ -29,5 +30,4 @@ function M = setSimulator(Nx, Ny, Ta, Tb, randAfterGoal)
     
 	%Defines horizon of simulation
     M.horizon = 3*(Nx+Ny);
-
 end
