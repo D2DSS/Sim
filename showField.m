5b4629a910c051fa0895a7f726e6bcb28060a6eb
Igor Oliveira Borges <igor.borges@hotmail.com>
2017-03-20 15:37:16 -0300

    Update showField.m

--- showField.m
+++ showField.m
@@ -1,18 +1,11 @@
+%% This a function of embed Graphical engine used optionally in simulator D2DSS
+% It is responsable for plotting static graphicals structures of soccer field
+% Central circle, goals lines, mid lines of field with aspects defined for parameters
+% function showField(placarA,placarB,Nx,Ny,tempo,rgbImage)
+%%
 function showField(placarA,placarB,Nx,Ny,tempo,rgbImage)
     gcf
-    annotation('textbox',...
-    [0 0 1 1],...
-    'String',{strcat('Placar: ',num2str(placarA),'X',num2str(placarB)),[strcat(num2str(tempo),' interacoes')]},...
-    'FontSize',14,...
-    'FontName','Arial',...
-    'Tag' , 'somethingUnique',...
-    'Color',[1 1 1]);
-
-    subplot(6,5,3);
-    s=imshow(A2);
-    set(s, 'AlphaData', alpha2);
-
-    %PARTE DO CAMPO
+    %Drawing field in plot 2d
     subplot(6,5,[6,30]);
     imshow(rgbImage,'XData',[0+.15 3*Nx],'YData',[0+.35 3*Ny]);
     axis on;grid on;
@@ -23,23 +16,21 @@ function showField(placarA,placarB,Nx,Ny,tempo,rgbImage)
     set(gca,'YTick',3:3:Ny*3);
     set(gca, 'XTickLabelMode', 'manual', 'XTickLabel', []);
     set(gca, 'YTickLabelMode', 'manual', 'YTickLabel', []);
-
-    %% Projeta os graficos em cima da matriz (circulo central, pequena area e gols)
     
-    %CIRCULO CENTRAL
+    %Central mid circle plot
     hold on;
     numPoints=100;
     radius=2.5;
     theta=linspace(0,5*pi,numPoints);
     rho=ones(1,numPoints)*radius;
-    [X,Y] = pol2cart(theta,rho); %funcao circular
+    [X,Y] = pol2cart(theta,rho); %circular function
 
     plot(X+Nx/2*3,Y+Ny/2*3,'k-','linewidth',2);
 
-    %LINHA CENTRAL   
+    %Central line in mid field
     line([Nx/2*3,Nx/2*3],[-1,size(rgbImage,1)]+1,'Color','k');
 
-    %AREAS DOS GOLS
+    %Goals areas
 
     golA=rectangle('position',[0 (floor(Ny/2)*3-3) 3 9],'LineWidth',2);
     set(golA,'FaceColor',[0.7,0.7,0.7]);
