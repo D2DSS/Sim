d19d3942f502f4ed0bfcbe4f47db804138b53f6f
Igor Oliveira Borges <igor.borges@hotmail.com>
2017-03-20 17:30:11 -0300

    Update setGame.m

--- setGame.m
+++ setGame.m
@@ -1,6 +1,8 @@
-function J = setGame( timer )    
-%This class encapsulates variables that set values for game configuration
-
+%%This class encapsulates variables that set values for game configuration in simulator D2DSS
+%       function J = setGame( timer )
+% Responsible for counting variables, timer, total, goalsA, goalsB, scoreA, scoreB
+%%
+function J = setGame( timer )
     J.timer = timer; %counter for simulation time control
     J.total=timer; %total time for a game
     J.goalsA = sparse(timer,1); %%sparse matrix for store goals in a game, 1 is for team A and B is -1. Note that index is the current time of goal in simulation
