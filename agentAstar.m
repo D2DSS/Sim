7c131e98187ef313a7dedf52920d4a2eed5af80e
Igor Oliveira Borges <igor.borges@hotmail.com>
2017-03-21 09:51:52 -0300

    Update agentAstar.m

--- agentAstar.m
+++ agentAstar.m
@@ -1,10 +1,18 @@
+%% Rules with A* search implemented in simulator D2DSS
+% Responsible for make a good static policy in game soccer
+% Based in using ifs for defining rules and making a search A* to find and include more rules 
+%       function G = agentAstar(limit,M)
+%%
+
 classdef agentAstar < handle
     properties
-        M
-        coord
-        limit
+        M % problem settings
+        coord %array of positions of players and ball
+        limit %limit max for A* search treee
     end
     methods
+    
+%Constructor of agent of type agentAstar            
         function G = agentAstar(limit,M)
             G.M = M;
 
@@ -13,18 +21,18 @@ classdef agentAstar < handle
             for i=1:(M.Ta-1)
                 G.coord{i} = floor(G.M.Nx*(1:i)/(i+1));
             end
-        end
-        
-        
+        end       
+
+%Function responsible for choose next best state based on rules strategy
         function sNext = nextState(G,s,a,Map)
             switch a
-                case 1 %Norte
+                case 1 %Move north
                     sNext = s+[0, -1];
-                case 2 % Sul
+                case 2 %Move south
                     sNext = s+[0, 1];
-                case 3 % Leste
+                case 3 %Move east
                     sNext = s+[1, 0];
-                case 4 % Oeste
+                case 4 %Move west
                     sNext = s+[-1, 0];
             end
             sNext = min(sNext,[G.M.Nx, G.M.Ny]);
@@ -34,15 +42,15 @@ classdef agentAstar < handle
                 sNext = s;
             end
         end
-        
+
+%Function with an additional planning using search A* concepts
         function [d a] = planning(G,pos,goal,Map,depth)
             border = zeros(G.M.Nx,G.M.Ny);
             analyzed = zeros(G.M.Nx,G.M.Ny);
             h = inf(G.M.Nx,G.M.Ny);
             f = zeros(G.M.Nx,G.M.Ny);
             actions = zeros(G.M.Nx,G.M.Ny);
-            
-            
+              
             border(pos(1),pos(2)) = 1;
             h(pos(1),pos(2)) = norm(pos-goal,1);
             f(pos(1),pos(2)) = 0;
@@ -106,36 +114,36 @@ classdef agentAstar < handle
             a = randi(4);
         end
         
-        
+%Pre planning for more a set of rules extended more actions than 4 basics of moves        
         function aa = prePlanning(G, dist, positions,aa,goal,Map,BallorPos)
             [d a] = min(dist);
             pos = positions(a,:);
             undecided = true;
             if d == 0
                 if ~BallorPos
-                    aa(a)=13; % Tackle
+                    aa(a)=13; %tackle for recovery ball
                     undecided = false;
                 else
                     positions(a,:) = 0;
                     if pos(2)<(ceil(G.M.Ny/2)-G.M.goalWidth) && max(abs(pos-[G.M.Nx (ceil(G.M.Ny/2)-G.M.goalWidth)])) <= G.M.ProbBallMoveDiagonal*G.M.KickRange 
-                        aa(a) = 10; % Chute Suldeste
+                        aa(a) = 10; %kick Southeast
                         undecided = false;
                     elseif pos(2)>(ceil(G.M.Ny/2)+G.M.goalWidth) && max(abs(pos-[G.M.Nx (ceil(G.M.Ny/2)+G.M.goalWidth)])) <= G.M.ProbBallMoveDiagonal*G.M.KickRange 
-                        aa(a) = 9; % Chute Nordeste
+                        aa(a) = 9; %kick Northeast
                         undecided = false;
                     elseif  pos(2)>=(ceil(G.M.Ny/2)-G.M.goalWidth) && pos(2)<=(ceil(G.M.Ny/2)+G.M.goalWidth) && (G.M.Nx-pos(1)) <= G.M.ProbBallWithPlayer*G.M.KickRange || any(positions(1:G.M.Ta,1)>pos(1))
-                        aa(a) = 7; % Passe ou Chute Leste
+                        aa(a) = 7; %ball pass or kick East
                         undecided = false;
                     elseif pos(2)>=(ceil(G.M.Ny/2)-G.M.goalWidth) && pos(2)<=(ceil(G.M.Ny/2)+G.M.goalWidth)
-                        goal = [min(pos(1)+G.limit,G.M.Nx) pos(2)]; % Go Leste
+                        goal = [min(pos(1)+G.limit,G.M.Nx) pos(2)]; %go East
                     elseif pos(2)<(ceil(G.M.Ny/2)-G.M.goalWidth)
-                        goal = [min(pos(1)+G.limit,G.M.Nx) (ceil(G.M.Ny/2)-G.M.goalWidth)];% Go Suldeste
+                        goal = [min(pos(1)+G.limit,G.M.Nx) (ceil(G.M.Ny/2)-G.M.goalWidth)];%go Southeast
                     elseif pos(2)>(ceil(G.M.Ny/2)+G.M.goalWidth)
-                        goal = [min(pos(1)+G.limit,G.M.Nx) (ceil(G.M.Ny/2)+G.M.goalWidth)];% Go Nordeste
+                        goal = [min(pos(1)+G.limit,G.M.Nx) (ceil(G.M.Ny/2)+G.M.goalWidth)];%go Northeast
                     end
                 end
             end    
-                
+            %if not could decide, keep with next rules    
             if undecided
                 if goal(1) < pos(1)
                     intX = max(goal(1),pos(1)-G.limit):pos(1);
@@ -152,34 +160,34 @@ classdef agentAstar < handle
                 if (sum(sum(Map(intX,intY))) == 1) && ~BallorPos || (sum(sum(Map(intX,intY))) == 0) && BallorPos;
                     d = abs(goal-pos);
                     if d(1)>d(2)
-                        if goal(1)>pos(1) %Leste
+                        if goal(1)>pos(1) %East
                             aa(a) = 3;
-                        else %Oeste
+                        else %West
                             aa(a) = 4;
                         end
                     elseif d(1)<d(2)
-                        if goal(2)>pos(2) %Sul
+                        if goal(2)>pos(2) %South
                             aa(a) = 2;
-                        else %Sul
+                        else %North
                             aa(a) = 1;
                         end
                     else
                         if rand > 0.5
-                            if goal(1)>pos(1) %Leste
+                            if goal(1)>pos(1) %East
                                 aa(a) = 3;
-                            else %Oeste
+                            else %West
                                 aa(a) = 4;
                             end
                         else
-                            if goal(2)>pos(2) %Sul
+                            if goal(2)>pos(2) %South
                                 aa(a) = 2;
-                            else %Sul
+                            else %North
                                 aa(a) = 1;
                             end
                         end
                     end
                 else
-                    [d act] = G.planning(pos,goal,Map,5);
+                    [d act] = G.planning(pos,goal,Map,5); % A* call for find new rules for take in decision for actual position in state
                     if act > 0
                         aa(a) = act;
                     else
@@ -190,7 +198,7 @@ classdef agentAstar < handle
             
         end
         
-        
+%Mapping state in action
         function aa = action(G,s)
             ball = s(1:2);
             Map = zeros(G.M.Nx,G.M.Ny);
@@ -212,7 +220,7 @@ classdef agentAstar < handle
                 end
             else
                  
-                % Quem vai para a bola?
+                %Decides who will go to ball?
 
                 dist = sum(abs(positions(1:G.M.Ta,:) - ones(G.M.Ta,1)*ball),2);
                 dist(aa~=0) = inf;
@@ -222,7 +230,7 @@ classdef agentAstar < handle
                 
             end
             
-            % Quem vai para cada posicao?
+            %Decides who will keep in each direction?
             
             for j=randperm(G.M.Ta-1)
 
@@ -236,7 +244,7 @@ classdef agentAstar < handle
                 dist = sum(abs(positions(1:G.M.Ta,:) - ones(G.M.Ta,1)*ball),2);
                 dist(aa~=0) = inf;
                 
-                aa = prePlanning(G, dist, positions,aa,ball,Map,false);
+                aa = prePlanning(G, dist, positions,aa,ball,Map,false); %call for a pre planning step for using all first static rules
 
 
             end
