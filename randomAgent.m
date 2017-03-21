85bb04fdcd39ee217ae0a439c007e88339c5fe2d
Igor Oliveira Borges <igor.borges@hotmail.com>
2017-03-21 09:55:59 -0300

    Update randomAgent.m

--- randomAgent.m
+++ randomAgent.m
@@ -1,14 +1,20 @@
+%% Random Agent implemented in simulator D2DSS
+% Responsible for take a random actions of provided max actions index
+%       function G = randomAgent(A)
+%%
 classdef randomAgent < handle
     properties
         A
     end
-    methods      
+    methods
+
+%Constructor of agent of type randomAgent  
         function G = randomAgent(A)
-            G.A = A;
+            G.A = A; %receive max value of valid actions for randomize
         end
 
         function a = action(G)
-            a = randi(G.A);
+            a = randi(G.A); %uses rand of integer function for take one of range valid actions
         end
     end
 end
